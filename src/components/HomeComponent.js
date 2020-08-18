21b177ceb7e9c50747ed01bb41dc8cac8a5219a2
maxmccomb <maxwellmccomb16@augustana.edu>
2020-08-17 19:53:34 -0500

    added a tags

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -22,22 +22,28 @@ class HomePage extends Component {
                     <Col>
                 <CardGroup>
                 <Card className="card-home bg-dark text-center hover_container">
+                <a href="/mymusic" className="a-home">
                         <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title><h2>Music</h2></Card.Title>
                         </Card.ImgOverlay>
+                        </a>
                     </Card>
                     <Card className="card-home bg-dark text-center hover_container">
+                    <a href="/aboutme" className="a-home">
                         <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title><h2>About Me</h2></Card.Title>
                         </Card.ImgOverlay>
+                        </a>
                     </Card>
                     <Card className="card-home bg-dark text-center hover_container">
+                    <a href="/events" className="a-home">
                         <Card.Img className="card-img-home" src={require("../assets/eventsimg.png")} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title><h2>Events</h2></Card.Title>
                         </Card.ImgOverlay>
+                        </a>
                     </Card>
                 </CardGroup>
                 </Col>
@@ -46,22 +52,25 @@ class HomePage extends Component {
                     <Col>
                 <CardGroup>
                 <Card className="card-home bg-dark text-center hover_container">
+                <a href="/supportme" className="a-home">
                         <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title><h2>Support Me</h2></Card.Title>
                         </Card.ImgOverlay>
+                        </a>
                     </Card>
                     <Card className="card-home bg-dark text-center hover_container">
-                        <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>Social Media</h2></Card.Title>
-                        </Card.ImgOverlay>
+                        <Card.Body>
+                            <div className="card_social" ><h3 className="h3_social">Social Media</h3></div>
+                        </Card.Body>
                     </Card>
                     <Card className="card-home bg-dark text-center hover_container">
+                    <a href="/store" className="a-home">
                         <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
                         <Card.ImgOverlay>
                             <Card.Title><h2>Store</h2></Card.Title>
                         </Card.ImgOverlay>
+                        </a>
                     </Card>
                 </CardGroup>
                 </Col>
