ccb1b9380a6c26cf9cebc83e652f02258675fc2a
maxmccomb <maxwellmccomb16@augustana.edu>
2020-08-17 20:40:43 -0500

    added social media buttons

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,6 +6,7 @@ import {
     Row,
     Col
 } from "react-bootstrap";
+import { SocialMediaIconsReact } from 'social-media-icons-react';
 import '../styling/custom_styling.css';
 
 
@@ -20,60 +21,71 @@ class HomePage extends Component {
                 </Row>
                 <Row>
                     <Col>
-                <CardGroup>
-                <Card className="card-home bg-dark text-center hover_container">
-                <a href="/mymusic" className="a-home">
-                        <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>Music</h2></Card.Title>
-                        </Card.ImgOverlay>
-                        </a>
-                    </Card>
-                    <Card className="card-home bg-dark text-center hover_container">
-                    <a href="/aboutme" className="a-home">
-                        <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>About Me</h2></Card.Title>
-                        </Card.ImgOverlay>
-                        </a>
-                    </Card>
-                    <Card className="card-home bg-dark text-center hover_container">
-                    <a href="/events" className="a-home">
-                        <Card.Img className="card-img-home" src={require("../assets/eventsimg.png")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>Events</h2></Card.Title>
-                        </Card.ImgOverlay>
-                        </a>
-                    </Card>
-                </CardGroup>
-                </Col>
+                        <CardGroup>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <a href="/mymusic" className="a-home">
+                                    <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h2>Music</h2></Card.Title>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <a href="/aboutme" className="a-home">
+                                    <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h2>About Me</h2></Card.Title>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <a href="/events" className="a-home">
+                                    <Card.Img className="card-img-home" src={require("../assets/eventsimg.png")} alt="Card image" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h2>Events</h2></Card.Title>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                        </CardGroup>
+                    </Col>
                 </Row>
                 <Row>
                     <Col>
-                <CardGroup>
-                <Card className="card-home bg-dark text-center hover_container">
-                <a href="/supportme" className="a-home">
-                        <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>Support Me</h2></Card.Title>
-                        </Card.ImgOverlay>
-                        </a>
-                    </Card>
-                    <Card className="card-home bg-dark text-center hover_container">
-                        <Card.Body>
-                            <div className="card_social" ><h3 className="h3_social">Social Media</h3></div>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-home bg-dark text-center hover_container">
-                    <a href="/store" className="a-home">
-                        <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title><h2>Store</h2></Card.Title>
-                        </Card.ImgOverlay>
-                        </a>
-                    </Card>
-                </CardGroup>
-                </Col>
+                        <CardGroup>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <a href="/supportme" className="a-home">
+                                    <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h2>Support Me</h2></Card.Title>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <Card.Body>
+                                    <Row className="justify-content-md-center">
+                                        <div >
+                                            <h3 className="h3_social">Social Media</h3>
+                                        </div>
+
+                                    </Row>
+                                    <Row className="justify-content-md-center">
+                                        <SocialMediaIconsReact icon="facebook" url="https://www.facebook.com/kevinphoenixmusic/" iconSize="10" size="50"/>
+                                        <SocialMediaIconsReact icon="youtube" url="https://www.youtube.com/user/TheXenonPhoenix" iconSize="10" size="50"/>
+                                        <SocialMediaIconsReact icon="instagram" url="https://facebook.com" iconSize="10" size="50"/>
+                                        <SocialMediaIconsReact icon="twitter" url="https://facebook.com" iconSize="10" size="50"/>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                            <Card className="card-home bg-dark text-center hover_container">
+                                <a href="/store" className="a-home">
+                                    <Card.Img className="card-img-home" src={require("../assets/crop_output.jpg")} alt="Card image" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h2>Store</h2></Card.Title>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                        </CardGroup>
+                    </Col>
                 </Row>
             </Container>
         );
